cad8ee8dca39f19fa57de600354dc1569eed0e03
Ferran Fuentes <ferran.fuentes@estudiantat.upc.edu>
2023-03-31 14:50:50 +0200

    Añadidos campos Matricula, Usuario y Contraseña en la interfaz

--- app/src/main/java/com/example/obd2pti/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/obd2pti/ui/notifications/NotificationsFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.obd2pti.databinding.FragmentNotificationsBinding
+import java.math.BigInteger
+import java.security.MessageDigest
 
 class NotificationsFragment : Fragment() {
 
@@ -32,6 +37,26 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val editTextMatricula: EditText = binding.editTextMatricula
+        val editTextUsuario: EditText = binding.editTextUsuario
+        val editTextPassword: EditText = binding.editTextPassword
+        val loginButton: Button = binding.loginbutton
+
+        var matricula: String
+        var usuario: String
+        var password: String
+
+        loginButton.setOnClickListener{
+            matricula = editTextMatricula.text.toString()
+            usuario = editTextUsuario.text.toString()
+            password = editTextPassword.text.toString()
+            Toast.makeText(context, "Matricula: $matricula, Usuario: $usuario, Password: $password", Toast.LENGTH_SHORT).show()
+            var md = MessageDigest.getInstance("SHA-256")
+            val passwordHash = BigInteger(1, md.digest(password.toByteArray())).toString(16).padStart(32, '0')
+            Toast.makeText(context, "Password Hash: $passwordHash", Toast.LENGTH_SHORT).show()
+        }
+
         return root
     }
 
